03ecb5f89ffacbedc0b2fc8d2eb5ae748c42dd2e
Homepage <koestens@t-online.de>
2025-11-04 17:21:36 +0100

    V1.0
    Erster Upload

---
+++ index_htm_files/1.js
@@ -0,0 +1,46 @@
+window.local_simpleGallery5Columns_5_data = [
+{
+ dimpage: "Y",
+ showshadow: "Y"
+},
+{
+ image: "index_htm_files/595.jpg",
+ thumbnail: "index_htm_files/596.jpg"
+},
+{
+ image: "index_htm_files/597.jpg",
+ thumbnail: "index_htm_files/598.jpg"
+},
+{
+ image: "index_htm_files/599.jpg",
+ thumbnail: "index_htm_files/600.jpg"
+},
+{
+ image: "index_htm_files/601.jpg",
+ thumbnail: "index_htm_files/602.jpg"
+},
+{
+ image: "index_htm_files/603.jpg",
+ thumbnail: "index_htm_files/604.jpg"
+},
+{
+ image: "index_htm_files/605.jpg",
+ thumbnail: "index_htm_files/606.jpg"
+},
+{
+ image: "index_htm_files/607.jpg",
+ thumbnail: "index_htm_files/608.jpg"
+},
+{
+ image: "index_htm_files/609.jpg",
+ thumbnail: "index_htm_files/610.jpg"
+},
+{
+ image: "index_htm_files/611.jpg",
+ thumbnail: "index_htm_files/612.jpg"
+},
+{
+ image: "index_htm_files/613.jpg",
+ thumbnail: "index_htm_files/614.jpg"
+}
+];
