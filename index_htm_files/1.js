a5f6a85307c7bc344d1c182812d0dda5885ab2c4
Homepage <koestens@t-online.de>
2025-11-06 22:03:16 +0100

    V1.02
    Eine darstellung

--- index_htm_files/1.js
+++ index_htm_files/1.js
@@ -4,43 +4,49 @@ window.local_simpleGallery5Columns_5_data = [
  showshadow: "Y"
 },
 {
- image: "index_htm_files/977.jpg",
- thumbnail: "index_htm_files/978.jpg"
+ image: "index_htm_files/1145.jpg",
+ thumbnail: "index_htm_files/1146.jpg"
 },
 {
- image: "index_htm_files/979.jpg",
- thumbnail: "index_htm_files/980.jpg"
+ image: "index_htm_files/1147.jpg",
+ thumbnail: "index_htm_files/1148.jpg"
 },
 {
- image: "index_htm_files/981.jpg",
- thumbnail: "index_htm_files/982.jpg"
+ image: "index_htm_files/1149.jpg",
+ thumbnail: "index_htm_files/1150.jpg"
 },
 {
- image: "index_htm_files/983.jpg",
- thumbnail: "index_htm_files/984.jpg"
+ image: "index_htm_files/1151.jpg",
+ thumbnail: "index_htm_files/1152.jpg"
 },
 {
- image: "index_htm_files/985.jpg",
- thumbnail: "index_htm_files/986.jpg"
+ image: "index_htm_files/1153.jpg",
+ thumbnail: "index_htm_files/1154.jpg"
 },
 {
- image: "index_htm_files/987.jpg",
- thumbnail: "index_htm_files/988.jpg"
+ image: "index_htm_files/1155.jpg",
+ thumbnail: "index_htm_files/1156.jpg"
 },
 {
- image: "index_htm_files/989.jpg",
- thumbnail: "index_htm_files/990.jpg"
+ image: "index_htm_files/1157.jpg",
+ thumbnail: "index_htm_files/1158.jpg"
 },
 {
- image: "index_htm_files/991.jpg",
- thumbnail: "index_htm_files/992.jpg"
+ image: "index_htm_files/1159.jpg",
+ thumbnail: "index_htm_files/1160.jpg"
 },
 {
- image: "index_htm_files/993.jpg",
- thumbnail: "index_htm_files/994.jpg"
+ image: "index_htm_files/1161.jpg",
+ thumbnail: "index_htm_files/1162.jpg"
 },
 {
- image: "index_htm_files/995.jpg",
- thumbnail: "index_htm_files/996.jpg"
+ image: "index_htm_files/1163.jpg",
+ thumbnail: "index_htm_files/1164.jpg"
+},
+{
+},
+{
+},
+{
 }
 ];
